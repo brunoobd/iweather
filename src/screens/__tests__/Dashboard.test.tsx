d614da0c40ca633219cabc87689e8d55f3702dd6
Bruno Duarte <bruno.duarte314@gmail.com>
2025-01-27 20:21:31 -0300

    Adjust Dashboard test

--- src/screens/__tests__/Dashboard.test.tsx
+++ src/screens/__tests__/Dashboard.test.tsx
@@ -22,11 +22,9 @@ describe("Screen: Dashboard", () => {
     jest.spyOn(api, "get").mockResolvedValue({ data: mockWeatherAPIResponse });
 
     const { findByText } = render(<Dashboard />);
-    const cityName = await waitFor(() =>
-      findByText(/rio de janeiro/i, {}, { timeout: 3000 })
+    await waitFor(() =>
+      expect(findByText(/rio de janeiro/i, {}, { timeout: 3000 })).toBeTruthy()
     );
-
-    expect(cityName).toBeTruthy();
   });
 
   it("should show another selected weather city", async () => {
